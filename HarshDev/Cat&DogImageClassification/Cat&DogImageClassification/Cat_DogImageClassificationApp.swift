7f4de83393a7a69248b47117dd42586d54a2c45f
BATCH02L1 <batch02l1@Admins-iMac.local>
2025-12-11 15:08:26 +0530

    Model Train

---
+++ HarshDev/Cat&DogImageClassification/Cat&DogImageClassification/Cat_DogImageClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_DogImageClassificationApp.swift
+//  Cat&DogImageClassification
+//
+//  Created by BATCH02L1 on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Cat_DogImageClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

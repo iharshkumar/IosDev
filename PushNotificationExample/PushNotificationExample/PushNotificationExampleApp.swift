3fec4dd0cb74ca97fe62efab7cc020241ca5a0e4
BATCH02L1 <batch02l1@Admins-iMac.local>
2025-12-12 12:11:53 +0530

    PushNotificationExample

---
+++ PushNotificationExample/PushNotificationExample/PushNotificationExampleApp.swift
@@ -0,0 +1,56 @@
+//
+//  PushNotificationExampleApp.swift
+//  PushNotificationExample
+//
+//  Created by BATCH02L1 on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationExampleApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self)var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+    init(){
+        registerNotificationPermission()
+    }
+    
+    
+    func registerNotificationPermission(){
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options:[.alert, .sound, .badge]){
+            granted, error in
+            if granted{
+                DispatchQueue.main.async{
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            
+            }else{
+                print("Push Notification permission granted : \(String(describing:error?.localizedDescription))")
+            }
+        }
+    }
+}
+
+class AppDelegate: NSObject,UIApplicationDelegate,UNUserNotificationCenterDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey:Any]?)->Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+}
---
+++ PushNotificationExample/PushNotificationExampleTests/PushNotificationExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationExampleTests.swift
+//  PushNotificationExampleTests
+//
+//  Created by BATCH02L1 on 12/12/25.
+//
+
+import Testing
+@testable import PushNotificationExample
+
+struct PushNotificationExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
